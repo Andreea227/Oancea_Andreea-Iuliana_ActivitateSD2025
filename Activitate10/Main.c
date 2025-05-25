6a2701b7469d842217a87a105d9ecd8805be2a3d
Andreea Oancea <oanceaandreea61@gmail.com>
2025-05-25 22:17:48 +0300

    Modificari alocare memorie

--- Activitate10/Main.c
+++ Activitate10/Main.c
@@ -13,48 +13,65 @@ struct Biblioteca {
 
 struct Biblioteca initializare(int cod, const char* nume, unsigned int nrCarti, const float* preturi, float suprafata) {
 	struct Biblioteca b;
+
 	b.cod = cod;
 	b.nrCarti = nrCarti;
 	b.suprafata = suprafata;
-	b.nume = malloc((strlen(nume) + 1) * sizeof(char));
-	strcpy(b.nume, nume);
-	b.preturi = malloc(sizeof(float) * nrCarti);
-	for (int i = 0; i < nrCarti; i++) {
-		b.preturi[i] = preturi[i];
+	b.nume = calloc((strlen(nume) + 1), sizeof(char));
+	if (b.nume != NULL) {
+		strcpy(b.nume, nume);
 	}
+
+	b.preturi = calloc(nrCarti, sizeof(float));
+	if (b.preturi != NULL) {
+		for (int i = 0; i < nrCarti; i++) {
+			b.preturi[i] = preturi[i];
+		}
+	}
+
 	return b;
 }
 
 void afisare(struct Biblioteca b) {
 	printf("Cod: %d. Suprafata: %.2f mp. Nume: %s\n", b.cod, b.suprafata, b.nume);
 	printf("Preturi carti: ");
+
 	for (int i = 0; i < b.nrCarti; i++) {
 		printf("%.2f; ", b.preturi[i]);
 	}
+
 	printf("\n");
 }
 
 struct Biblioteca copiereBiblioteca(struct Biblioteca b) {
 	struct Biblioteca copie;
+
 	copie.cod = b.cod;
 	copie.nrCarti = b.nrCarti;
 	copie.suprafata = b.suprafata;
-	copie.nume = malloc((strlen(b.nume) + 1) * sizeof(char));
-	strcpy(copie.nume, b.nume);
-	copie.preturi = malloc(sizeof(float) * b.nrCarti);
-	for (int i = 0; i < b.nrCarti; i++) {
-		copie.preturi[i] = b.preturi[i];
+	copie.nume = calloc((strlen(b.nume) + 1), sizeof(char));
+	if (copie.nume != NULL) {
+		strcpy(copie.nume, b.nume);
 	}
+
+	copie.preturi = calloc(b.nrCarti, sizeof(float));
+	if (copie.preturi != NULL) {
+		for (int i = 0; i < b.nrCarti; i++) {
+			copie.preturi[i] = b.preturi[i];
+		}
+	}
+
 	return copie;
 }
 
 void push_biblioteca(struct Biblioteca b, struct Biblioteca** biblioteci, int* nrBiblioteci) {
 	(*nrBiblioteci)++;
-	struct Biblioteca* nou = malloc(sizeof(struct Biblioteca) * (*nrBiblioteci));
+	struct Biblioteca* nou = calloc((*nrBiblioteci), sizeof(struct Biblioteca));
 
 	for (int i = 0; i < (*nrBiblioteci) - 1; i++) {
 		nou[i] = (*biblioteci)[i]; // shallow copy
 	}
+
 	nou[(*nrBiblioteci) - 1] = copiereBiblioteca(b);
 
 	free(*biblioteci);
@@ -63,9 +80,11 @@ void push_biblioteca(struct Biblioteca b, struct Biblioteca** biblioteci, int* n
 
 int totalCarti(struct Biblioteca* biblioteci, int nrBiblioteci) {
 	int total = 0;
+
 	for (int i = 0; i < nrBiblioteci; i++) {
 		total += biblioteci[i].nrCarti;
 	}
+
 	return total;
 }
 
@@ -95,6 +114,7 @@ int main() {
 		free(biblioteci[i].nume);
 		free(biblioteci[i].preturi);
 	}
+
 	free(biblioteci);
 	free(b1.nume);
 	free(b1.preturi);
