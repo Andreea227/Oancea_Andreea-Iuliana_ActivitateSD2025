4a3a74e0dccbae2a52b3b4f4789243b44f627952
Andreea Oancea <oanceaandreea61@gmail.com>
2025-05-25 22:06:38 +0300

    Noua implementare

--- Activitate9/Main.c
+++ Activitate9/Main.c
@@ -1,20 +1,83 @@
-// Activitate9.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <iostream>
+typedef struct Pizzerie Pizzerie;
+typedef struct Nod Nod;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct Pizzerie {
+	char* nume;
+	int nrComenziZilnic;
+	float pretMediuPizza;
+};
+
+struct Nod {
+	struct Pizzerie pizzerie;
+	struct Nod* next;
+};
+
+struct Pizzerie initializarePizzerie(const char* nume, int nrComenziZilnic, float pretMediuPizza) {
+	struct Pizzerie p;
+	p.nume = malloc(strlen(nume) + 1);
+	strcpy(p.nume, nume);
+	p.nrComenziZilnic = nrComenziZilnic;
+	p.pretMediuPizza = pretMediuPizza;
+	return p;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void afisarePizzerie(struct Pizzerie p) {
+	printf("Pizzerie: %s | Comenzi/zi: %d | Pret mediu: %.2f\n", p.nume, p.nrComenziZilnic, p.pretMediuPizza);
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void afisareLista(Nod* cap) {
+	while (cap != NULL) {
+		afisarePizzerie(cap->pizzerie);
+		cap = cap->next;
+	}
+}
+
+void pushBack(Nod** head, struct Pizzerie p) {
+	Nod* nou = malloc(sizeof(Nod));
+	nou->pizzerie = p;
+	nou->next = NULL;
+	if (*head == NULL) {
+		*head = nou;
+	}
+	else {
+		Nod* temp = *head;
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+		temp->next = nou;
+	}
+}
+
+Nod* pushFront(Nod* head, struct Pizzerie p) {
+	Nod* nou = malloc(sizeof(Nod));
+	nou->pizzerie = p;
+	nou->next = head;
+	return nou;
+}
+
+int main() {
+	struct Pizzerie p1 = initializarePizzerie("Pizza Napoli", 120, 25.5f);
+	struct Pizzerie p2 = initializarePizzerie("Pizza Roma", 95, 23.0f);
+
+	Nod* lista = NULL;
+	lista = pushFront(lista, p1);
+	lista = pushFront(lista, p2);
+	pushBack(&lista, initializarePizzerie("Pizza Milano", 80, 27.3f));
+
+	afisareLista(lista);
+
+	// Eliberare memorie
+	while (lista != NULL) {
+		free(lista->pizzerie.nume);
+		Nod* tmp = lista;
+		lista = lista->next;
+		free(tmp);
+	}
+
+	return 0;
+}
