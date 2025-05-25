2863edf99347f99f1595903a7aeaefa2b1557281
Andreea Oancea <oanceaandreea61@gmail.com>
2025-05-25 22:03:43 +0300

    Adaugare implementare noua

--- Activitate8/Main.c
+++ Activitate8/Main.c
@@ -1,20 +1,64 @@
-// Activitate8.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <iostream>
+struct Librarie {
+	char* nume;
+	int nrTipuriCarti;
+	float* preturi;
+	int* nrExemplare;
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct Librarie creareLibrarie(char* nume, int nrTipuriCarti, float* preturi, int* nrExemplare) {
+	struct Librarie librarie;
+
+	librarie.nume = calloc(strlen(nume) + 1, sizeof(char));
+	if (librarie.nume != NULL) {
+		strcpy(librarie.nume, nume);
+	}
+	librarie.nrTipuriCarti = nrTipuriCarti;
+	librarie.preturi = calloc(nrTipuriCarti, sizeof(float));
+	librarie.nrExemplare = calloc(nrTipuriCarti, sizeof(int));
+
+	for (int i = 0; i < nrTipuriCarti; i++) {
+		librarie.preturi[i] = preturi[i];
+		librarie.nrExemplare[i] = nrExemplare[i];
+	}
+
+	return librarie;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void afisareLibrarie(struct Librarie l) {
+	printf("\nNumele librariei: %s", l.nume);
+	printf("\nNumar tipuri carti: %d", l.nrTipuriCarti);
+	printf("\nPreturi si stocuri:\n");
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	for (int i = 0; i < l.nrTipuriCarti; i++) {
+		printf("Carte %d - %.2f lei, %d exemplare\n", i + 1, l.preturi[i], l.nrExemplare[i]);
+	}
+}
+
+void editareNume(struct Librarie* librarie, char* numeNou) {
+	free(librarie->nume);
+	librarie->nume = calloc(strlen(numeNou) + 1, sizeof(char));
+	if (librarie->nume != NULL) {
+		strcpy(librarie->nume, numeNou);
+	}
+}
+
+void main() {
+	float preturi[] = { 45.5, 29.9 };
+	int exemplare[] = { 10, 20 };
+
+	struct Librarie l = creareLibrarie("Carturesti", 2, preturi, exemplare);
+	afisareLibrarie(l);
+
+	editareNume(&l, "Humanitas");
+	afisareLibrarie(l);
+
+	// Eliberare memorie
+	free(l.nume);
+	free(l.preturi);
+	free(l.nrExemplare);
+}
